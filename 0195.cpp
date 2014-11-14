53fd6381a412e1963d9ff3f5ae8be19cb2a0cae1
IvSur <neogenick@gmail.com>
2014-11-14 12:22:11 +0800

    0195.cpp
    Задача № 0195.
    http://acm.dvpion.ru/?main=task&id_task=195

---
+++ 0195.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+	  freopen("output.txt", "w", stdout);
+
+    int n, a, b;
+
+    cin >> n >> a >> b;
+    cout << n * a * b * 2;
+
+    return 0;
+}
