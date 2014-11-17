da533590533b854faaea11b5506a455f357cff58
IvSur <neogenick@gmail.com>
2014-11-17 17:24:30 +0800

    0272.cpp
    http://acm.dvpion.ru/index.asp?main=task&id_task=272

---
+++ 0272.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int a,b,c,min,max;
+    c = 0;
+    max = -10000;
+    min = 10000;
+    while (cin >> a)
+    {
+        c = c + 1;
+        if (c % 2 == 0)
+        {
+            if (a >= max)
+				max = a;
+        }
+        else
+        {
+            if (a <= min)
+				min = a;
+        }
+    }
+
+    cout << max + min;
+
+    return 0;
+}
